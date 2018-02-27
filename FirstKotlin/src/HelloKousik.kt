a6185de4c54563dee79957dae33443477b506633
araveti-Git <araveti.veerasekhar@googlemail.com>
2018-02-27 13:58:10 +0530

    First Kotlin Commit

---
+++ FirstKotlin/src/HelloKousik.kt
@@ -0,0 +1,14 @@
+package demo
+
+fun main (args: Array<String>) {
+    println ("Kousik Age is")
+    println (12)
+
+    val myName = "Veera"
+    var myAge = 43
+
+    myAge += 1
+
+    println(myName + " Age is " + myAge)
+
+}
